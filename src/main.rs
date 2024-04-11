41e78600c69df01efab81bc83ffdb44dfbd46823
mablr <59505383+mablr@users.noreply.github.com>
2024-04-11 14:21:53 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use zmq::{Context, Message, PULL};
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+enum EventType {
+    IO,
+    CT,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Event {
+    event_type: EventType,
+    // common fields
+    file_descriptor: i32,
+    container: String,
+    // IO fields
+    method: Option<String>,
+    // CT fields
+    ressource_identifier: Option<String>, // file path, host socket
+    input: Option<bool>, // eg. read privilege on a file
+    output: Option<bool>, // eg. write privilege on a file
+    remote: Option<bool>, // eg. communication channel trough network
+}
+
+fn main() {
+    let context = Context::new();
+    let socket = context.socket(PULL).unwrap();
+    assert!(socket.bind("tcp://*:5555").is_ok());
+
+    loop {
+        let mut buffer = Message::new();
+        socket.recv(&mut buffer, 0).unwrap();
+
+        let event: Event = rmp_serde::from_slice(&buffer).unwrap();
+        match event.event_type {
+            EventType::IO => println!("IO Event: FD {} | {} on {}", event.file_descriptor, event.method.unwrap(), event.container), 
+            EventType::CT => println!("CT Event: FD {} | IN {} | OUT {} | REMOTE {} | {} @ {}", event.file_descriptor, event.input.unwrap(), event.output.unwrap(), event.remote.unwrap(), event.container, event.ressource_identifier.unwrap()),
+        };
+    }
+}
