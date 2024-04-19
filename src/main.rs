ab9e299ef01092d216ece755188aacd28dd2fd5b
mablr <59505383+mablr@users.noreply.github.com>
2024-04-19 15:57:41 +0200

    Introducing gRPC

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,34 @@
-use zmq::Context;
-use serde::{Serialize, Deserialize};
+use tonic::{transport::Server, Request, Response, Status};
+use trace2e::{Ct, Io, Ack, trace2e_server::{Trace2e, Trace2eServer}};
 
-#[derive(Debug, Serialize, Deserialize)]
-enum EventType {
-    IO,
-    CT,
+pub mod trace2e {
+  tonic::include_proto!("trace2e");
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Event {
-    event_type: EventType,
-    // common fields
-    file_descriptor: i32,
-    container: String,
-    // IO fields
-    method: Option<String>,
-    // CT fields
-    ressource_identifier: Option<String>, // file path, host socket
-    input: Option<bool>, // eg. read privilege on a file
-    output: Option<bool>, // eg. write privilege on a file
-    remote: Option<bool>, // eg. communication channel trough network
-}
+#[derive(Debug, Default)]
+pub struct Trace2eService {}
 
-fn main() {
-    let context = Context::new();
-    let socket = context.socket(zmq::REP).unwrap();
-    assert!(socket.bind("tcp://*:5555").is_ok());
-
-    loop {
-        let buffer = socket.recv_bytes(0).unwrap();
-        let event: Event = rmp_serde::from_slice(&buffer).unwrap();
-        match event.event_type {
-            EventType::IO => println!("IO Event: FD {} | {} on {}", event.file_descriptor, event.method.unwrap(), event.container), 
-            EventType::CT => println!("CT Event: FD {} | IN {} | OUT {} | REMOTE {} | {} @ {}", event.file_descriptor, event.input.unwrap(), event.output.unwrap(), event.remote.unwrap(), event.container, event.ressource_identifier.unwrap()),
-        };
-        let _ = socket.send("ack", 0);
+#[tonic::async_trait]
+impl Trace2e for Trace2eService {
+    async fn ct_event(&self, request: Request<Ct>) -> Result<Response<Ack>, Status> {
+        let r = request.into_inner();
+        println!("CT Event: FD {} | IN {} | OUT {} | REMOTE {} | {} @ {}", r.file_descriptor, r.input, r.output, r.remote, r.container, r.resource_identifier);
+        Ok(Response::new(trace2e::Ack { authorization: { true }}))
     }
+    async fn io_event(&self, request: Request<Io>) -> Result<Response<Ack>, Status> {
+        let r = request.into_inner();
+        println!("IO Event: FD {} | {} on {}", r.file_descriptor, r.method, r.container);
+        Ok(Response::new(trace2e::Ack { authorization: { true }}))
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let address = "[::1]:8080".parse().unwrap();
+    let trace2e_service = Trace2eService::default();
+
+    Server::builder().add_service(Trace2eServer::new(trace2e_service))
+        .serve(address)
+        .await?;
+    Ok(())       
 }
