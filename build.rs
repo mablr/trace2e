d42a77274d7e8f4c4ff7426e9a4b5beac7ffc4ca
mablr <59505383+mablr@users.noreply.github.com>
2024-05-29 14:15:14 +0200

    Renaming proto files

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main () -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(true)
-        .file_descriptor_set_path("target/trace2e_descriptor.bin")
-        .compile(&["./proto/trace2e.proto"], &["./proto"])?;
+        .file_descriptor_set_path("target/p2m_descriptor.bin")
+        .compile(&["./proto/p2m.proto"], &["./proto"])?;
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
 pub mod p2m {
     tonic::include_proto!("p2m");
-    pub const FILE_DESCRIPTOR_SET: &[u8] = include_bytes!("../target/trace2e_descriptor.bin");
+    pub const FILE_DESCRIPTOR_SET: &[u8] = include_bytes!("../target/p2m_descriptor.bin");
 }
