43e922afb50f13b4cac76b6a3dd431e53526f1b4
mablr <59505383+mablr@users.noreply.github.com>
2024-08-01 12:11:42 +0200

    Testing all Identifier enum members

--- src/containers/tests.rs
+++ src/containers/tests.rs
@@ -1,12 +1,22 @@
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+
 use super::*;
 
 #[test]
 fn containers_manager_register() {
     let mut containers_manager = ContainersManager::default();
     let id1 = Identifier::File("/test/path/file1.txt".to_string());
-    let id2 = Identifier::File("/test/path/file2.txt".to_string());
+    let id2 = Identifier::Stream(
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12312),
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8081),
+    );
+    let id3 = Identifier::Process(1);
     assert_eq!(containers_manager.register(id1.clone()), true);
     assert_eq!(containers_manager.register(id2.clone()), true);
+    assert_eq!(containers_manager.register(id1.clone()), false);
+    assert_eq!(containers_manager.register(id3.clone()), true);
+    assert_eq!(containers_manager.register(id3.clone()), false);
+    assert_eq!(containers_manager.register(id2.clone()), false);
     assert_eq!(containers_manager.register(id1.clone()), false);
 }
 
@@ -14,7 +24,10 @@ fn containers_manager_register() {
 fn containers_manager_try_reservation() {
     let mut containers_manager = ContainersManager::default();
     let id1 = Identifier::File("/test/path/file1.txt".to_string());
-    let id2 = Identifier::File("/test/path/file2.txt".to_string());
+    let id2 = Identifier::Stream(
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 12312),
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8081),
+    );
     assert_eq!(containers_manager.register(id1.clone()), true);
     assert_eq!(containers_manager.try_reservation(id1.clone()), Ok(true));
     assert_eq!(containers_manager.try_reservation(id1.clone()), Ok(false));
