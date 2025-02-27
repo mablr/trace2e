a1789837a36546ba42eb29ddad43004c25ab8637
mablr <59505383+mablr@users.noreply.github.com>
2025-02-27 17:20:13 +0100

    Examples for stde2e

--- stde2e/build.rs
+++
@@ -1,6 +0,0 @@
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .file_descriptor_set_path("../target/p2m_descriptor.bin")
-        .compile(&["../proto/p2m_api.proto"], &["../proto"])?;
-    Ok(())
-}
---
+++ stde2e/examples/file_forwarder.rs
@@ -0,0 +1,41 @@
+use clap::Parser;
+use std::{fs::File, io::{Read, Write}};
+
+/// File manipulation program that reads from input and writes to output
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Input file path
+    #[arg(short, long)]
+    input: Option<String>,
+
+    /// Output file path
+    #[arg(short, long)]
+    output: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Parse command line arguments
+    let args = Args::parse();
+
+    // Read from input file if provided
+    let content = if let Some(path) = args.input {
+        let mut file = File::open(path)?;
+        let mut buffer = String::new();
+        file.read_to_string(&mut buffer)?;
+        buffer
+    } else {
+        return Err("No input file provided".into());
+    };
+
+    // Write to output file if provided
+    if let Some(path) = args.output {
+        let mut file = File::create(path)?;
+        file.write_all(content.as_bytes())?;
+    } else {
+        return Err("No output file provided".into());
+    }
+
+    Ok(())
+}
