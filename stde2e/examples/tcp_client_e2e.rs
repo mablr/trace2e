e44dab0d4984b19a92eed4a475f167ef84b410f6
mablr <59505383+mablr@users.noreply.github.com>
2025-02-28 11:52:11 +0100

    fine tuning  examples

--- stde2e/examples/tcp_client_e2e.rs
+++ stde2e/examples/tcp_client_e2e.rs
@@ -3,11 +3,14 @@ use std::time::Instant;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let start_time = Instant::now();
+    let start_connect_time = Instant::now();
     let mut stream = TcpStream::connect("127.0.0.1:8888").unwrap();
+    let connect_time = start_connect_time.elapsed();
+    std::thread::sleep(std::time::Duration::from_millis(100));
     let mut buf = [0; 16];
+    let start_read_time = Instant::now();
     stream.read(&mut buf).unwrap();
-    let end_time = start_time.elapsed();
-    println!("\"stde2e\": \"{:?}\",", end_time);
+    let read_time = start_read_time.elapsed();
+    println!("\"stde2e\": \"{:?}\"", connect_time + read_time);
     Ok(())
 }
--- stde2e/examples/tcp_server.rs
+++ stde2e/examples/tcp_server.rs
@@ -2,7 +2,7 @@ use std::{io::Write, net::TcpListener};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
+    let listener = TcpListener::bind("0.0.0.0:8888").unwrap();
     match listener.accept() {
         Ok((mut s, _)) => {
             s.write(b"Hello, world!\n").unwrap();
