04ac3936acaceb382610c205cde19dda1648e537
mablr <59505383+mablr@users.noreply.github.com>
2025-03-20 15:37:05 +0100

    Lighter tracing strategy

--- demo/stde2e/scripts/bench_std_file.sh
+++ demo/stde2e/scripts/bench_std_file.sh
@@ -2,18 +2,18 @@
 
 get_timings() {
     local enroll_p_sent=$(echo "$1" | grep "\[P2M\] P-> local_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    local enroll_m_begin=$(echo "$2" | grep "\[P2M\] ->M local_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    local enroll_m_done=$(echo "$2" | grep "\[P2M\] <-M local_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local enroll_m_begin=$(echo "$1" | grep "\[P2M\] ->M local_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local enroll_m_done=$(echo "$1" | grep "\[P2M\] <-M local_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
     local enroll_p_recv=$(echo "$1" | grep "\[P2M\] P<- local_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
 
     local io_request_p_sent=$(echo "$1" | grep "\[P2M\] P-> io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    local io_request_m_begin=$(echo "$2" | grep "\[P2M\] ->M io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    local io_request_m_done=$(echo "$2" | grep "\[P2M\] <-M io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local io_request_m_begin=$(echo "$1" | grep "\[P2M\] ->M io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local io_request_m_done=$(echo "$1" | grep "\[P2M\] <-M io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
     local io_request_p_recv=$(echo "$1" | grep "\[P2M\] P<- io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
 
     local io_report_p_sent=$(echo "$1" | grep "\[P2M\] P-> io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    local io_report_m_begin=$(echo "$2" | grep "\[P2M\] ->M io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    local io_report_m_done=$(echo "$2" | grep "\[P2M\] <-M io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local io_report_m_begin=$(echo "$1" | grep "\[P2M\] ->M io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local io_report_m_done=$(echo "$1" | grep "\[P2M\] <-M io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
     local io_report_p_recv=$(echo "$1" | grep "\[P2M\] P<- io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
 
     local enroll_req_time=$(echo "$enroll_m_begin - $enroll_p_sent" | bc)
@@ -42,33 +42,23 @@ i=0
 while [ $i -lt $NB_ITER ];do
     sudo truncate -s 0 $(docker inspect --format='{{.LogPath}}' stde2e)
 
-    P_LOG=$(echo "RUST_LOG=info /file_forwarder_e2e -i /dev/null -o /dev/zero" | docker exec -i stde2e bash | ansi2txt)
-    M_LOG=$(docker logs stde2e | ansi2txt)
-
-    # echo -n "$P_LOG" > p_log.txt
-    # echo -n "$M_LOG" > m_log.txt
-
-    # P_LOG=$(cat p_log.txt)
-    # M_LOG=$(cat m_log.txt)
+    LOG=$(echo 'RUST_LOG="trace2e_middleware::p2m_service=info" /trace2e_middleware & middleware=$! && RUST_LOG=info /file_forwarder_e2e -i /dev/null -o /dev/zero; kill "$middleware"' | docker exec -i stde2e bash | ansi2txt)
 
     # if same amount of local_enroll, io_request, io_report in P_LOG and M_LOG
-    if [ $(echo "$P_LOG" | grep "local_enroll" | wc -l) -ne $(echo "$M_LOG" | grep "local_enroll" | wc -l)\
-        -o $(echo "$P_LOG" | grep "io_request" | wc -l) -ne $(echo "$M_LOG" | grep "io_request" | wc -l)\
-        -o $(echo "$P_LOG" | grep "io_report" | wc -l) -ne $(echo "$M_LOG" | grep "io_report" | wc -l)\
-        -o $(echo "$P_LOG" | grep "\[P2M\]" | wc -l) -ne "12"\
-        -o $(echo "$M_LOG" | grep "\[P2M\]" | wc -l) -ne "12"\
-        -o $(echo "$M_LOG" | grep "root@.*#" | wc -l) -ne "0" ];
+    if [ $(echo "$LOG" | grep "local_enroll" | wc -l) -ne "8"\
+        -o $(echo "$LOG" | grep "io_request" | wc -l) -ne "8"\
+        -o $(echo "$LOG" | grep "io_report" | wc -l) -ne "8"\
+        -o $(echo "$LOG" | grep "\[P2M\]" | wc -l) -ne "24"\
+        -o $(echo "$LOG" | grep "root@.*#" | wc -l) -ne "0" ];
     then
         echo "Error: incorrect logs, retrying..." >&2
         continue
     fi
 
-    P_LOG_READ=$(echo "$P_LOG" | grep -v "\[DEMO\]" | head -n 6)
-    M_LOG_READ=$(echo "$M_LOG" | head -n 6)
-    P_LOG_WRITE=$(echo "$P_LOG" | grep -v "\[DEMO\]" | tail -n 6)
-    M_LOG_WRITE=$(echo "$M_LOG"| tail -n 6)
+    LOG_READ=$(echo "$LOG" | head -n 12)
+    LOG_WRITE=$(echo "$LOG"| tail -n 12)
 
-    echo "$(get_timings "$P_LOG_READ" "$M_LOG_READ");$(get_timings "$P_LOG_WRITE" "$M_LOG_WRITE")"
+    echo "$(get_timings "$LOG_READ");$(get_timings "$LOG_WRITE")"
 
     i=$((i+1))
 done
--- demo/stde2e/scripts/bench_std_tcpstream.sh
+++ demo/stde2e/scripts/bench_std_tcpstream.sh
@@ -1,128 +1,88 @@
 #!/bin/bash
 
-get_timings_write() {
+get_timings() {
+    local enroll_p_sent=$(echo "$1" | grep "\[P2M\] P-> remote_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local enroll_m_begin=$(echo "$1" | grep "\[P2M\] ->M remote_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local enroll_m_done=$(echo "$1" | grep "\[P2M\] <-M remote_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local enroll_p_recv=$(echo "$1" | grep "\[P2M\] P<- remote_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
 
+    local io_request_p_sent=$(echo "$1" | grep "\[P2M\] P-> io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local io_request_m_begin=$(echo "$1" | grep "\[P2M\] ->M io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local io_request_m_done=$(echo "$1" | grep "\[P2M\] <-M io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local io_request_p_recv=$(echo "$1" | grep "\[P2M\] P<- io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
 
-    echo "$enroll_req_time" "$enroll_processing_time" "$enroll_ack_time" "$io_request_req_time" "$io_request_processing_time" "$io_request_ack_time" "$io_time" "$io_report_req_time" "$io_report_processing_time" "$io_report_ack_time" "$reserve_req_time" "$reserve_processing_time" "$reserve_ack_time" "$sync_prov_req_time" "$sync_prov_processing_time" "$sync_prov_ack_time"
+    local io_report_p_sent=$(echo "$1" | grep "\[P2M\] P-> io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local io_report_m_begin=$(echo "$1" | grep "\[P2M\] ->M io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local io_report_m_done=$(echo "$1" | grep "\[P2M\] <-M io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local io_report_p_recv=$(echo "$1" | grep "\[P2M\] P<- io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+
+    local enroll_req_time=$(echo "$enroll_m_begin - $enroll_p_sent" | bc)
+    local enroll_processing_time=$(echo "$enroll_m_done - $enroll_m_begin" | bc)
+    local enroll_ack_time=$(echo "$enroll_p_recv - $enroll_m_done" | bc)
+
+    local io_request_req_time=$(echo "$io_request_m_begin - $io_request_p_sent" | bc)
+    local io_request_processing_time=$(echo "$io_request_m_done - $io_request_m_begin" | bc)
+    local io_request_ack_time=$(echo "$io_request_p_recv - $io_request_m_done" | bc)
+
+    local io_time=$(echo "$io_request_p_recv - $io_request_p_sent" | bc)
+
+    local io_report_req_time=$(echo "$io_report_m_begin - $io_report_p_sent" | bc)
+    local io_report_processing_time=$(echo "$io_report_m_done - $io_report_m_begin" | bc)
+    local io_report_ack_time=$(echo "$io_report_p_recv - $io_report_m_done" | bc)
+
+    echo "$enroll_req_time;$enroll_processing_time;$enroll_ack_time;$io_request_req_time;$io_request_processing_time;$io_request_ack_time;$io_time;$io_report_req_time;$io_report_processing_time;$io_report_ack_time"
+}
+
+get_timings_m2m() {
+    local write_reserve_l_sent=$(echo "$1" | grep "\[M2M\] LM-> reserve" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local write_reserve_r_begin=$(echo "$1" | grep "\[M2M\] ->RM reserve" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local write_reserve_r_done=$(echo "$1" | grep "\[M2M\] <-RM reserve" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local write_reserve_l_recv=$(echo "$1" | grep "\[M2M\] LM<- reserve" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+
+    local write_sync_prov_l_sent=$(echo "$1" | grep "\[M2M\] LM-> sync_prov" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local write_sync_prov_r_begin=$(echo "$1" | grep "\[M2M\] ->RM sync_prov" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local write_sync_prov_r_done=$(echo "$1" | grep "\[M2M\] <-RM sync_prov" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+    local write_sync_prov_l_recv=$(echo "$1" | grep "\[M2M\] LM<- sync_prov" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
+
+    local write_reserve_req_time=$(echo "$write_reserve_r_begin - $write_reserve_l_sent" | bc)
+    local write_reserve_processing_time=$(echo "$write_reserve_r_done - $write_reserve_r_begin" | bc)
+    local write_reserve_ack_time=$(echo "$write_reserve_l_recv - $write_reserve_r_done" | bc)
+
+    local write_sync_prov_req_time=$(echo "$write_sync_prov_r_begin - $write_sync_prov_l_sent" | bc)
+    local write_sync_prov_processing_time=$(echo "$write_sync_prov_r_done - $write_sync_prov_r_begin" | bc)
+    local write_sync_prov_ack_time=$(echo "$write_sync_prov_l_recv - $write_sync_prov_r_done" | bc)
+
+    echo "$write_reserve_req_time;$write_reserve_processing_time;$write_reserve_ack_time;$write_sync_prov_req_time;$write_sync_prov_processing_time;$write_sync_prov_ack_time"
 }
 
 NB_ITER=100
 
-echo "write_enroll_req_time;write_enroll_processing_time;write_enroll_ack_time;write_io_request_req_time;write_io_request_processing_time;write_io_request_ack_time;write_io_time;write_io_report_req_time;write_io_report_processing_time;write_io_report_ack_time;write_reserve_req_time;write_reserve_processing_time;write_reserve_ack_time;write_sync_prov_req_time;write_sync_prov_processing_time;write_sync_prov_ack_time;read_enroll_req_time;read_enroll_processing_time;read_enroll_ack_time;read_io_request_req_time;read_io_request_processing_time;read_io_request_ack_time;read_io_time;read_io_report_req_time;read_io_report_processing_time;read_io_report_ack_time"
+echo "read_enroll_req_time;read_enroll_processing_time;read_enroll_ack_time;read_io_request_req_time;read_io_request_processing_time;read_io_request_ack_time;read_io_time;read_io_report_req_time;read_io_report_processing_time;read_io_report_ack_time;write_enroll_req_time;write_enroll_processing_time;write_enroll_ack_time;write_io_request_req_time;write_io_request_processing_time;write_io_request_ack_time;write_io_time;write_io_report_req_time;write_io_report_processing_time;write_io_report_ack_time;write_reserve_req_time;write_reserve_processing_time;write_reserve_ack_time;write_sync_prov_req_time;write_sync_prov_processing_time;write_sync_prov_ack_time"
 
 i=0
 while [ $i -lt $NB_ITER ];do
 
-    sudo truncate -s 0 $(docker inspect --format='{{.LogPath}}' stde2e) $(docker inspect --format='{{.LogPath}}' stde2e_server)
+    LOG=$(echo 'RUST_LOG="trace2e_middleware::p2m_service=info" /trace2e_middleware & middleware=$! && RUST_LOG=trace2e_client=info /tcp_server_e2e; kill "$middleware"' | docker exec -i stde2e_server bash 2>/dev/null | ansi2txt & echo 'RUST_LOG="trace2e_middleware::p2m_service=info" /trace2e_middleware & middleware=$! && RUST_LOG=trace2e_client=info /tcp_client_e2e stde2e_server:8888; kill "$middleware"' | docker exec -i stde2e bash  2>/dev/null | ansi2txt)
 
-    P_LOG=$(echo "RUST_LOG=info /tcp_server_e2e" | docker exec -i stde2e_server bash 2>/dev/null | ansi2txt & echo "RUST_LOG=info /tcp_client_e2e stde2e_server:8888" | docker exec -i stde2e bash  2>/dev/null | ansi2txt)
-    M_LOG=$(docker compose logs | ansi2txt | sed "s/.*| //")
-
-    # echo -n "$P_LOG" > p_log.txt
-    # echo -n "$M_LOG" > m_log.txt
-
-    # P_LOG=$(cat p_log.txt)
-    # M_LOG=$(cat m_log.txt)
-
-    if [ $(echo "$P_LOG" | grep "remote_enroll" | wc -l) -ne $(echo "$M_LOG" | grep "remote_enroll" | wc -l)\
-        -o $(echo "$P_LOG" | grep "io_request" | wc -l) -ne $(echo "$M_LOG" | grep "io_request" | wc -l)\
-        -o $(echo "$P_LOG" | grep "io_report" | wc -l) -ne $(echo "$M_LOG" | grep "io_report" | wc -l)\
-        -o $(echo "$P_LOG" | grep "\[P2M\]" | wc -l) -ne "12"\
-        -o $(echo "$M_LOG" | grep "\[P2M\]" | wc -l) -ne "12"\
-        -o $(echo "$M_LOG" | grep "\[M2M\]" | wc -l) -ne "8"\
-        -o $(echo "$M_LOG" | grep "root@.*#" | wc -l) -ne "0" ];
+    LOG_M2M=$(echo 'RUST_LOG="trace2e_middleware::m2m_service=info,trace2e_middleware::traceability=info" /trace2e_middleware & middleware=$! && /tcp_server_e2e; kill "$middleware"' | docker exec -i stde2e_server bash 2>/dev/null | ansi2txt & echo 'RUST_LOG="trace2e_middleware::m2m_service=info,trace2e_middleware::traceability=info" /trace2e_middleware & middleware=$! && /tcp_client_e2e stde2e_server:8888; kill "$middleware"' | docker exec -i stde2e bash  2>/dev/null | ansi2txt)
+    if [ $(echo "$LOG" | grep "remote_enroll" | wc -l) -ne "8"\
+        -o $(echo "$LOG" | grep "io_request" | wc -l) -ne "8"\
+        -o $(echo "$LOG" | grep "io_report" | wc -l) -ne "8"\
+        -o $(echo "$LOG" | grep "root@.*#" | wc -l) -ne "0"\
+        -o $(echo "$LOG_M2M" | grep "reserve" | wc -l) -ne "4"\
+        -o $(echo "$LOG_M2M" | grep "sync_prov" | wc -l) -ne "4"\
+        -o $(echo "$LOG_M2M" | grep "root@.*#" | wc -l) -ne "0" ];
     then
         echo "Error: incorrect logs, retrying..." >&2
         continue
     fi
 
-    FD_TO_READ=$(echo "$P_LOG" | grep "P-> remote_enroll" | grep "8888])$" | awk -F', ' '{print $2}')
+    FD_TO_READ=$(echo "$LOG" | grep "P-> remote_enroll" | grep "8888])$" | awk -F', ' '{print $2}')
 
-    P_LOG_READ=$(echo "$P_LOG" | grep -v "\[DEMO\]" | grep "$FD_TO_READ")
-    M_LOG_READ=$(echo "$M_LOG" | grep "$FD_TO_READ")
-    P_LOG_WRITE=$(echo "$P_LOG" | grep -v "\[DEMO\]" | grep -v "$FD_TO_READ")
-    M_LOG_WRITE=$(echo "$M_LOG"| grep -v "$FD_TO_READ")
+    LOG_READ=$(echo "$LOG" | grep "$FD_TO_READ")
+    LOG_WRITE=$(echo "$LOG" | grep -v "$FD_TO_READ")
 
-
-    read_enroll_p_sent=$(echo "$P_LOG_READ" | grep "\[P2M\] P-> remote_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    read_enroll_m_begin=$(echo "$M_LOG_READ" | grep "\[P2M\] ->M remote_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    read_enroll_m_done=$(echo "$M_LOG_READ" | grep "\[P2M\] <-M remote_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    read_enroll_p_recv=$(echo "$P_LOG_READ" | grep "\[P2M\] P<- remote_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-
-    read_io_request_p_sent=$(echo "$P_LOG_READ" | grep "\[P2M\] P-> io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    read_io_request_m_begin=$(echo "$M_LOG_READ" | grep "\[P2M\] ->M io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    read_io_request_m_done=$(echo "$M_LOG_READ" | grep "\[P2M\] <-M io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    read_io_request_p_recv=$(echo "$P_LOG_READ" | grep "\[P2M\] P<- io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-
-    read_io_report_p_sent=$(echo "$P_LOG_READ" | grep "\[P2M\] P-> io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    read_io_report_m_begin=$(echo "$M_LOG_READ" | grep "\[P2M\] ->M io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    read_io_report_m_done=$(echo "$M_LOG_READ" | grep "\[P2M\] <-M io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    read_io_report_p_recv=$(echo "$P_LOG_READ" | grep "\[P2M\] P<- io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-
-
-    read_enroll_req_time=$(echo "$read_enroll_m_begin - $read_enroll_p_sent" | bc)
-    read_enroll_processing_time=$(echo "$read_enroll_m_done - $read_enroll_m_begin" | bc)
-    read_enroll_ack_time=$(echo "$read_enroll_p_recv - $read_enroll_m_done" | bc)
-
-    read_io_request_req_time=$(echo "$read_io_request_m_begin - $read_io_request_p_sent" | bc)
-    read_io_request_processing_time=$(echo "$read_io_request_m_done - $read_io_request_m_begin" | bc)
-    read_io_request_ack_time=$(echo "$read_io_request_p_recv - $read_io_request_m_done" | bc)
-
-    read_io_time=$(echo "$read_io_report_p_sent - $read_io_request_p_recv" | bc)
-
-    read_io_report_req_time=$(echo "$read_io_report_m_begin - $read_io_report_p_sent" | bc)
-    read_io_report_processing_time=$(echo "$read_io_report_m_done - $read_io_report_m_begin" | bc)
-    read_io_report_ack_time=$(echo "$read_io_report_p_recv - $read_io_report_m_done" | bc)
-
-
-    write_enroll_p_sent=$(echo "$P_LOG_WRITE" | grep "\[P2M\] P-> remote_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_enroll_m_begin=$(echo "$M_LOG_WRITE" | grep "\[P2M\] ->M remote_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_enroll_m_done=$(echo "$M_LOG_WRITE" | grep "\[P2M\] <-M remote_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_enroll_p_recv=$(echo "$P_LOG_WRITE" | grep "\[P2M\] P<- remote_enroll" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    
-    write_io_request_p_sent=$(echo "$P_LOG_WRITE" | grep "\[P2M\] P-> io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_io_request_m_begin=$(echo "$M_LOG_WRITE" | grep "\[P2M\] ->M io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_io_request_m_done=$(echo "$M_LOG_WRITE" | grep "\[P2M\] <-M io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_io_request_p_recv=$(echo "$P_LOG_WRITE" | grep "\[P2M\] P<- io_request" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-
-    write_io_report_p_sent=$(echo "$P_LOG_WRITE" | grep "\[P2M\] P-> io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_io_report_m_begin=$(echo "$M_LOG_WRITE" | grep "\[P2M\] ->M io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_io_report_m_done=$(echo "$M_LOG_WRITE" | grep "\[P2M\] <-M io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_io_report_p_recv=$(echo "$P_LOG_WRITE" | grep "\[P2M\] P<- io_report" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-
-    write_reserve_l_sent=$(echo "$M_LOG_WRITE" | grep "\[M2M\] LM-> reserve" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_reserve_r_begin=$(echo "$M_LOG_WRITE" | grep "\[M2M\] ->RM reserve" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_reserve_r_done=$(echo "$M_LOG_WRITE" | grep "\[M2M\] <-RM reserve" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_reserve_l_recv=$(echo "$M_LOG_WRITE" | grep "\[M2M\] LM<- reserve" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-
-    write_sync_prov_l_sent=$(echo "$M_LOG_WRITE" | grep "\[M2M\] LM-> sync_prov" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_sync_prov_r_begin=$(echo "$M_LOG_WRITE" | grep "\[M2M\] ->RM sync_prov" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_sync_prov_r_done=$(echo "$M_LOG_WRITE" | grep "\[M2M\] <-RM sync_prov" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    write_sync_prov_l_recv=$(echo "$M_LOG_WRITE" | grep "\[M2M\] LM<- sync_prov" | awk '{print $1}' | xargs -I {} date -d "{}" +%s%N | cut -c 1-16)
-    
-    write_enroll_req_time=$(echo "$write_enroll_m_begin - $write_enroll_p_sent" | bc)
-    write_enroll_processing_time=$(echo "$write_enroll_m_done - $write_enroll_m_begin" | bc)
-    write_enroll_ack_time=$(echo "$write_enroll_p_recv - $write_enroll_m_done" | bc)
-
-    write_io_request_req_time=$(echo "$write_io_request_m_begin - $write_io_request_p_sent" | bc)
-    write_io_request_processing_time=$(echo "$write_reserve_l_sent - $write_io_request_m_begin + $write_io_request_m_done - $write_reserve_l_recv" | bc) # includes reserve time
-    write_io_request_ack_time=$(echo "$write_io_request_p_recv - $write_io_request_m_done" | bc)
-
-    write_io_time=$(echo "$write_io_report_p_sent - $write_io_request_p_recv" | bc)
-
-    write_io_report_req_time=$(echo "$write_io_report_m_begin - $write_io_report_p_sent" | bc)
-    write_io_report_processing_time=$(echo "$write_io_report_m_done - $write_io_report_m_begin" | bc) # includes sync_prov time
-    write_io_report_ack_time=$(echo "$write_io_report_p_recv - $write_io_report_m_done" | bc)
-
-    write_reserve_req_time=$(echo "$write_reserve_r_begin - $write_reserve_l_sent" | bc)
-    write_reserve_processing_time=$(echo "$write_reserve_r_done - $write_reserve_r_begin" | bc)
-    write_reserve_ack_time=$(echo "$write_reserve_l_recv - $write_reserve_r_done" | bc)
-
-    write_sync_prov_req_time=$(echo "$write_sync_prov_r_begin - $write_sync_prov_l_sent" | bc)
-    write_sync_prov_processing_time=$(echo "$write_sync_prov_r_done - $write_sync_prov_r_begin" | bc)
-    write_sync_prov_ack_time=$(echo "$write_sync_prov_l_recv - $write_sync_prov_r_done" | bc)
-
-    echo "$write_enroll_req_time;$write_enroll_processing_time;$write_enroll_ack_time;$write_io_request_req_time;$write_io_request_processing_time;$write_io_request_ack_time;$write_io_time;$write_io_report_req_time;$write_io_report_processing_time;$write_io_report_ack_time;$write_reserve_req_time;$write_reserve_processing_time;$write_reserve_ack_time;$write_sync_prov_req_time;$write_sync_prov_processing_time;$write_sync_prov_ack_time;$read_enroll_req_time;$read_enroll_processing_time;$read_enroll_ack_time;$read_io_request_req_time;$read_io_request_processing_time;$read_io_request_ack_time;$read_io_time;$read_io_report_req_time;$read_io_report_processing_time;$read_io_report_ack_time"
+    echo "$(get_timings "$LOG_READ");$(get_timings "$LOG_WRITE");$(get_timings_m2m "$LOG_M2M")"
     i=$((i+1))
 done
 
