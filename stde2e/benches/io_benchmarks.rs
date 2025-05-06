e9092a84fa0de37bd1cbe2ff4300b7e26f26c849
mablr <59505383+mablr@users.noreply.github.com>
2025-05-06 11:03:13 +0200

    TCP streams benchmarks

--- stde2e/benches/io_benchmarks.rs
+++ stde2e/benches/io_benchmarks.rs
@@ -1,97 +1,158 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use std::fs::File;
-use stde2e::fs::File as FileE2E;
 
-fn bench_read_e2e(c: &mut Criterion) {
-    let mut small_file = File::create("read_small.txt").unwrap();
+fn bench_file_read_e2e(c: &mut Criterion) {
+    let mut small_file = std::fs::File::create("read_small.txt").unwrap();
     std::io::Write::write_all(&mut small_file, &vec![1u8; 64]).unwrap();
-    let mut large_file = File::create("read_large.txt").unwrap();
+    let mut large_file = std::fs::File::create("read_large.txt").unwrap();
     std::io::Write::write_all(&mut large_file, &vec![1u8; 262144]).unwrap();
 
     let mut buf = vec![0u8; 262144];
-    let mut e2e_small_file = FileE2E::open("read_small.txt").unwrap();
-    let mut e2e_large_file: File = FileE2E::open("read_large.txt").unwrap();
+    let mut e2e_small_file = stde2e::fs::File::open("read_small.txt").unwrap();
+    let mut e2e_large_file = stde2e::fs::File::open("read_large.txt").unwrap();
 
-    c.bench_function("read_e2e_small_buffer", |b| {
+    c.bench_function("file_read_e2e_small", |b| {
         b.iter(|| {
             black_box(stde2e::io::Read::read_to_end(&mut e2e_small_file, &mut buf).unwrap());
         })
     });
 
-    c.bench_function("read_e2e_large_buffer", |b| {
+    c.bench_function("file_read_e2e_large", |b| {
         b.iter(|| {
             black_box(stde2e::io::Read::read_to_end(&mut e2e_large_file, &mut buf).unwrap());
         })
     });
 }
 
-fn bench_write_e2e(c: &mut Criterion) {
+fn bench_file_write_e2e(c: &mut Criterion) {
     let small_buf = vec![1u8; 64];
     let large_buf = vec![1u8; 262144];
-    let mut e2e_small_file = FileE2E::create("write_small.txt").unwrap();
-    let mut e2e_large_file = FileE2E::create("write_large.txt").unwrap();
+    let mut e2e_small_file = stde2e::fs::File::create("write_small.txt").unwrap();
+    let mut e2e_large_file = stde2e::fs::File::create("write_large.txt").unwrap();
 
-    c.bench_function("write_e2e_small_buffer", |b| {
+    c.bench_function("file_write_e2e_small", |b| {
         b.iter(|| {
             black_box(stde2e::io::Write::write_all(&mut e2e_small_file, &small_buf).unwrap());
         })
     });
 
-    c.bench_function("write_e2e_large_buffer", |b| {
+    c.bench_function("file_write_e2e_large", |b| {
         b.iter(|| {
             black_box(stde2e::io::Write::write_all(&mut e2e_large_file, &large_buf).unwrap());
         })
     });
 }
 
-fn bench_read_std(c: &mut Criterion) {
-    let mut small_file = File::create("read_small.txt").unwrap();
+fn bench_file_read_std(c: &mut Criterion) {
+    let mut small_file = std::fs::File::create("read_small.txt").unwrap();
     std::io::Write::write_all(&mut small_file, &vec![1u8; 64]).unwrap();
-    let mut large_file = File::create("read_large.txt").unwrap();
+    let mut large_file = std::fs::File::create("read_large.txt").unwrap();
     std::io::Write::write_all(&mut large_file, &vec![1u8; 262144]).unwrap();
 
     let mut buf = vec![0u8; 262144];
-    let mut small_file = File::open("read_small.txt").unwrap();
-    let mut large_file = File::open("read_large.txt").unwrap();
+    let mut small_file = std::fs::File::open("read_small.txt").unwrap();
+    let mut large_file = std::fs::File::open("read_large.txt").unwrap();
 
-    c.bench_function("read_std_small_buffer", |b| {
+    c.bench_function("file_read_std_small", |b| {
         b.iter(|| {
             black_box(std::io::Read::read_to_end(&mut small_file, &mut buf).unwrap());
         })
     });
 
-    c.bench_function("read_std_large_buffer", |b| {
+    c.bench_function("file_read_std_large", |b| {
         b.iter(|| {
             black_box(std::io::Read::read_to_end(&mut large_file, &mut buf).unwrap());
         })
     });
 }
 
-
-fn bench_write_std(c: &mut Criterion) {
+fn bench_file_write_std(c: &mut Criterion) {
     let small_buf = vec![1u8; 64];
     let large_buf = vec![1u8; 262144];
-    let mut small_file = File::create("write_small.txt").unwrap();
-    let mut large_file = File::create("write_large.txt").unwrap();
+    let mut small_file = std::fs::File::create("write_small.txt").unwrap();
+    let mut large_file = std::fs::File::create("write_large.txt").unwrap();
 
-    c.bench_function("write_std_small_buffer", |b| {
+    c.bench_function("file_write_std_small", |b| {
         b.iter(|| {
             black_box(std::io::Write::write_all(&mut small_file, &small_buf).unwrap());
         })
     });
 
-    c.bench_function("write_std_large_buffer", |b| {
+    c.bench_function("file_write_std_large", |b| {
         b.iter(|| {
             black_box(std::io::Write::write_all(&mut large_file, &large_buf).unwrap());
         })
     });
 }
 
+fn bench_tcp_stream_write_e2e(c: &mut Criterion) {
+    let buf = vec![1u8; 262144];
+
+    // Start server in a separate thread
+    let server = stde2e::net::TcpListener::bind("127.0.0.1:0").unwrap();
+    let addr = server.local_addr().unwrap();
+    
+    let server_thread = std::thread::spawn(move || {
+        let (mut stream, _) = server.accept().unwrap();
+        loop {
+            let mut read_buf = vec![0u8; 262144];
+            match std::io::Read::read(&mut stream, &mut read_buf) {
+                Ok(0) => break, // Connection closed
+                Ok(_) => continue,
+                Err(_) => break,
+            }
+        }
+    });
+
+    let mut client = stde2e::net::TcpStream::connect(addr).unwrap();
+
+    c.bench_function("tcp_stream_write_e2e", |b| {
+        b.iter(|| {
+            black_box(stde2e::io::Write::write_all(&mut client, &buf).unwrap());
+        })
+    });
+
+    drop(client); // Close the connection
+    server_thread.join().unwrap();
+}
+
+fn bench_tcp_stream_write_std(c: &mut Criterion) {
+    let buf = vec![1u8; 262144];
+
+    // Start server in a separate thread
+    let server = std::net::TcpListener::bind("127.0.0.1:0").unwrap();
+    let addr = server.local_addr().unwrap();
+    
+    let server_thread = std::thread::spawn(move || {
+        let (mut stream, _) = server.accept().unwrap();
+        loop {
+            let mut read_buf = vec![0u8; 262144];
+            match std::io::Read::read(&mut stream, &mut read_buf) {
+                Ok(0) => break, // Connection closed
+                Ok(_) => continue,
+                Err(_) => break,
+            }
+        }
+    });
+
+    let mut client = std::net::TcpStream::connect(addr).unwrap();
+
+    c.bench_function("tcp_stream_write_std", |b| {
+        b.iter(|| {
+            black_box(std::io::Write::write_all(&mut client, &buf).unwrap());
+        })
+    });
+
+    drop(client); // Close the connection
+    server_thread.join().unwrap();
+}
+
 criterion_group!(
     benches,
-    bench_read_e2e,
-    bench_write_e2e,
-    bench_read_std,
-    bench_write_std
+    bench_file_read_e2e,
+    bench_file_write_e2e,
+    bench_file_read_std,
+    bench_file_write_std,
+    bench_tcp_stream_write_e2e,
+    bench_tcp_stream_write_std
 );
 criterion_main!(benches);
