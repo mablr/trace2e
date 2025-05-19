7616fecf15896ea54fb374d68c52164b668713f0
mablr <59505383+mablr@users.noreply.github.com>
2025-05-19 11:41:47 +0200

    Reafactor/Upgrade dependencies

--- trace2e_client/build.rs
+++ trace2e_client/build.rs
@@ -1,6 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .file_descriptor_set_path("trace2e_descriptor.bin")
-        .compile(&["../proto/trace2e.proto"], &["../proto"])?;
+    tonic_build::compile_protos("../proto/trace2e.proto")?;
     Ok(())
 }
--- trace2e_middleware/build.rs
+++ trace2e_middleware/build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .file_descriptor_set_path("trace2e_descriptor.bin")
-        .compile(&["../proto/trace2e.proto"], &["../proto"])?;
+        .compile_protos(&["../proto/trace2e.proto"], &["../proto"])?;
     Ok(())
 }
