566658bd14f278a15d0be8bf428b36e59aaacfe5
Mablr <59505383+mablr@users.noreply.github.com>
2025-06-12 14:18:46 +0200

    ignore stde2e test

--- stde2e/tests/file.rs
+++ stde2e/tests/file.rs
@@ -3,6 +3,7 @@ use std::time::Instant;
 use stde2e::{fs::File, io::Write};
 
 #[test]
+#[ignore]
 fn stde2e_file_create() {
     let time = Instant::now();
     let mut f = File::create("test1.txt").unwrap();
--- stde2e/tests/stream.rs
+++ stde2e/tests/stream.rs
@@ -5,6 +5,7 @@ use stde2e::{
 use tokio::runtime::Handle;
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 4)]
+#[ignore]
 async fn stde2e_net_stream_instantiation() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8081")?;
     Handle::current().spawn(async move {
