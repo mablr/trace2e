19e4b848b9a8f854c200212f41fee4303e4506dd
mablr <59505383+mablr@users.noreply.github.com>
2025-08-07 14:01:53 +0200

    feat(it-tests): add complex distributed integration test + flowchart annotations

--- trace2e_middleware/src/tests/mod.rs
+++ trace2e_middleware/src/tests/mod.rs
@@ -20,6 +20,12 @@ use crate::{
 
 #[tokio::test]
 async fn integration_init_middleware() {
+    // flowchart LR
+    //     P1[Process1] -->|1| F(File)
+    //     P1[Process1] -->|2| S(Stream)
+    //     F -->|3| P1
+    //     S -->|4| P1
+
     #[cfg(feature = "trace2e_tracing")]
     crate::trace2e_tracing::init();
     let (_, mut p2m_service, _) = init_middleware("10.0.0.1".to_string(), None, M2mNop::default());
@@ -38,6 +44,13 @@ async fn integration_init_middleware() {
 
 #[tokio::test]
 async fn integration_spawn_loopback_middlewares() {
+    // flowchart LR
+    //     s1337on1@{ shape: h-cyl}
+    //     s1338on2@{ shape: h-cyl}
+    //     s1337on1["socket1337 on Node1"] --- s1338on2["socket1338 on Node2"]
+
+    //     P1on1["Process1 on Node1"] -- 1 --> s1337on1
+    //     s1338on2 -- 2 --> P1on2["Process1 on Node2"]
     #[cfg(feature = "trace2e_tracing")]
     crate::trace2e_tracing::init();
     let ips = vec!["10.0.0.1".to_string(), "10.0.0.2".to_string()];
@@ -69,6 +82,10 @@ async fn integration_spawn_loopback_middlewares() {
 
 #[tokio::test]
 async fn integration_o2m_local_provenance() {
+    // flowchart LR
+    //     F(File) -->|1| P1[Process1]
+    //     P1 -->|2| F
+
     #[cfg(feature = "trace2e_tracing")]
     crate::trace2e_tracing::init();
     let (_, mut p2m_service, mut o2m_service) =
@@ -109,7 +126,14 @@ async fn integration_o2m_local_provenance() {
 }
 
 #[tokio::test]
-async fn integration_o2m_remote_provenance() {
+async fn integration_o2m_remote_provenance_basic() {
+    // flowchart LR
+    //     s1337on1@{ shape: h-cyl}
+    //     s1338on2@{ shape: h-cyl}
+    //     s1337on1["socket1337 on Node1"] --- s1338on2["socket1338 on Node2"]
+
+    //     P1on1["Process1 on Node1"] -- 1 --> s1337on1
+    //     s1338on2 -- 2 --> P1on2["Process1 on Node2"]
     #[cfg(feature = "trace2e_tracing")]
     crate::trace2e_tracing::init();
     let ips = vec!["10.0.0.1".to_string(), "10.0.0.2".to_string()];
@@ -149,5 +173,111 @@ async fn integration_o2m_remote_provenance() {
             ("10.0.0.1".to_string(), HashSet::from([stream1.process()])),
             ("10.0.0.2".to_string(), HashSet::from([stream2.process()]))
         ])
+    );
+}
+
+#[tokio::test]
+async fn integration_o2m_remote_provenance_complex() {
+    // flowchart LR
+    //     s1337on1["socket1337 on Node1"] --- s1338on2["socket1338 on Node2"]
+    //     s1339on2["socket1339 on Node2"] --- s1340on3["socket1340 on Node3"]
+    //     s1337on1@{ shape: h-cyl}
+    //     s1338on2@{ shape: h-cyl}
+    //     s1339on2@{ shape: h-cyl}
+    //     s1340on3@{ shape: h-cyl}
+
+    //     P1on1["Process1 on Node1"]
+    //     P2on2["Process3 on Node2"]
+    //     P3on3["Process3 on Node3"]
+
+    //     F1_1_1[File1 opened by Process1@Node1]
+    //     F1_1_2[File2 opened by Process1@Node1]
+
+    //     F1_1_1 -- 1 --> P1on1
+    //     P1on1 -- 2 --> s1337on1
+    //     F1_1_2 -- 3 --> P1on1
+    //     s1338on2 -- 4 --> P2on2
+    //     P2on2 -- 5 --> s1339on2
+    //     s1340on3 -- 6 --> P3on3
+
+    #[cfg(feature = "trace2e_tracing")]
+    crate::trace2e_tracing::init();
+    let ips = vec![
+        "10.0.0.1".to_string(),
+        "10.0.0.2".to_string(),
+        "10.0.0.3".to_string(),
+    ];
+    let mut middlewares = spawn_loopback_middlewares(ips.clone())
+        .await
+        .into_iter()
+        .map(|(p2m, o2m)| {
+            (
+                ServiceBuilder::new()
+                    .layer(TimeoutLayer::new(Duration::from_millis(1)))
+                    .service(p2m),
+                o2m,
+            )
+        })
+        .collect::<VecDeque<_>>();
+
+    let (mut p2m_1, mut o2m_1) = middlewares.pop_front().unwrap();
+    let (mut p2m_2, mut o2m_2) = middlewares.pop_front().unwrap();
+    let (mut p2m_3, mut o2m_3) = middlewares.pop_front().unwrap();
+
+    let fd1_1_1 = FileMapping::new(1, 4, "/tmp/test1.txt");
+    let fd1_1_2 = FileMapping::new(1, 5, "/tmp/test2.txt");
+
+    local_enroll!(p2m_1, fd1_1_1);
+    local_enroll!(p2m_1, fd1_1_2);
+
+    let stream1_2 = StreamMapping::new(1, 3, "10.0.0.1:1337", "10.0.0.2:1338");
+    let stream2_1 = StreamMapping::new(2, 3, "10.0.0.2:1338", "10.0.0.1:1337");
+    let stream2_3 = StreamMapping::new(2, 4, "10.0.0.2:1339", "10.0.0.3:1340");
+    let stream3_2 = StreamMapping::new(3, 3, "10.0.0.3:1340", "10.0.0.2:1339");
+
+    remote_enroll!(p2m_1, stream1_2);
+    remote_enroll!(p2m_2, stream2_1);
+    remote_enroll!(p2m_2, stream2_3);
+    remote_enroll!(p2m_3, stream3_2);
+
+    read!(p2m_1, fd1_1_1);
+    write!(p2m_1, stream1_2);
+    read!(p2m_1, fd1_1_2);
+    read!(p2m_2, stream2_1);
+    write!(p2m_2, stream2_3);
+    read!(p2m_3, stream3_2);
+
+    assert_provenance!(
+        o2m_3,
+        stream3_2.process(), // P3on3
+        HashMap::from([
+            (
+                "10.0.0.1".to_string(),
+                HashSet::from([fd1_1_1.file(), fd1_1_1.process()])
+            ),
+            ("10.0.0.2".to_string(), HashSet::from([stream2_3.process()])),
+            ("10.0.0.3".to_string(), HashSet::from([stream3_2.process()]))
+        ])
+    );
+
+    assert_provenance!(
+        o2m_2,
+        stream2_3.process(), // P2on2 eq stream2_1.process()
+        HashMap::from([
+            (
+                "10.0.0.1".to_string(),
+                HashSet::from([fd1_1_1.file(), fd1_1_1.process()])
+            ),
+            ("10.0.0.2".to_string(), HashSet::from([stream2_1.process()])),
+        ])
+    );
+
+    assert_provenance!(
+        o2m_1,
+        stream1_2.process(), // P1on1 eq fd1_1_1.process() | fd1_1_2.process()
+        HashMap::from([(
+            "10.0.0.1".to_string(),
+            HashSet::from([fd1_1_1.file(), fd1_1_2.file(), fd1_1_1.process()])
+        ),])
     );
 }
